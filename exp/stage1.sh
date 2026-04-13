df33b15b702fd6444bc0c78593ddb9f4d966807f
Alex Nah <nyk7535@snu.ac.kr>
2026-04-13 06:15:15 +0000

    chore: update stage1 repo-id to converted image dataset

--- exp/stage1.sh
+++ exp/stage1.sh
@@ -10,5 +10,5 @@ python scripts/train_rl_token.py \
     --train.vla-finetune-alpha 1.0 \
     --train.batch-size 32 \
     --train.num-train-steps 3000 \
-    --repo-id Pick_up_the_pen_and_place_it_in_the_trashbin_lerobot \
+    --repo-id pick_pen_image \
     --data-transforms-fn rlt_openpi.policies.franka.config.three_camera_droid
